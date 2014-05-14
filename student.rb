3bb0c0e42f6826e89347d1ec543b6d0b9b09becb
David Becerra <DavidBecerra84@gmail.com>
2014-05-13 21:44:00 -0700

    add junior students

--- student.rb
+++ student.rb
@@ -1,23 +1,31 @@
 class Student
+  attr_reader :first_name, :last_name, :grade
 
-	attr_reader :first_name, :last_name, :grade
-	def initialize(first_name, last_name, grade)
-		@first_name = first_name
-		@last_name = last_name
-		@grade = grade
-	end
+  def initialize(first_name, last_name, grade)
+    @first_name = first_name
+    @last_name = last_name
+    @grade = grade
+  end
 
-	def senior?
-		grade == 12
-	end
+  def senior?
+    grade == 12
+  end
 
-	def to_s
-		"#{last_name}, #{first_name}"
-	end
+  def junior?
+    grade == 11
+  end
+
+  def to_s
+    "#{last_name}, #{first_name}"
+  end
 end
 
 def seniors(students)
-	students.select { |student| student.senior? }
+  students.select { |student| student.senior? }
+end
+
+def juniors(students)
+  students.select { |student| student.junior? }
 end
 
 fred = Student.new("Fred", "James", 12)
@@ -25,6 +33,13 @@ sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
 all_students = [fred, sarah, jack]
 
+puts "Seniors"
 seniors(all_students).each do |student|
-	puts student
+  puts "* #{student}"
 end
+
+puts "Juniors"
+juniors(all_students).each do |student|
+  puts "* #{student}"
+end
+
